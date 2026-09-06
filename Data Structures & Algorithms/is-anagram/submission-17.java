2c05c1b20aaacdb215ca7ebcc4a2a7e773dd6835
Bullen Kosa <kosabullen@gmail.com>
2026-09-05 23:50:14 -0700

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> counts = new HashMap<>();
+
+        for (char letter : s.toCharArray()) {
+            counts.merge(letter, 1, Integer::sum);
+        }
+
+        for (char letter : t.toCharArray()) {
+            counts.merge(letter, -1, Integer::sum);
+            if (counts.get(letter) < 0) return false;
+        }
+
+      return true;
+
+
+
+    }
+}
